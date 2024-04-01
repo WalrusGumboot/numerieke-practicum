96e8fd4689ed525865611b8b3439d5f4829bc175
Simeon Duwel <tameimpalafan123@gmail.com>
2024-04-01 14:34:09 +0200

    oeps

--- lu_decomp.m
+++ lu_decomp.m
@@ -3,10 +3,12 @@ function [L, U] = lu_decomp(A)
 % L: een onderdriehoeksmatrix met diagonaalelementen gelijk aan 1
 % U: een bovendriehoeksmatrix
 
+n = length(A);
+
 L = eye(n);
 U = zeros(n);
 
-for i = 1:length(A)
+for i = 1:n
     product = L * U;
     U(i, i:end) = A(i, i:end) - product(i, i:end);
     L(i+1:end, i) = ( A(i+1:end, i) - product(i+1:end, i) ) / U(i, i);
