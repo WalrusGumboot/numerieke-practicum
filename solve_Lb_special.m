6fd2fc577ed3ca76b1ddfb4b3fdc5e73f6260ff6
MaraLevrau <mara.levrau@gmail.com>
2024-04-14 12:31:58 +0200

    deftige versie solve_Lb_special

--- solve_Lb_special.m
+++ solve_Lb_special.m
@@ -5,8 +5,6 @@ function [y] = solve_Lb_special( L, b)
 % y: de oplossing van het stelsel Ly = b
 n = length(b);
 y = zeros(1, n);
-for k=1:n-1
-    y(k) = b(k);
-end
+y(1:n-1) = b(1:n-1);
 y(n) = b(n) - L(n, 1:n-1)*b(1:n-1, :);
 end
